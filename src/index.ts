abedb96e94d0a15ee82229aeeedf0fa19c879fd1
diepmyduong@outlook.com.vn <diepmyduong@outlook.com.vn>
2021-11-03 11:53:58 +0700

    Feat: Batch Async

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { TypedEmitter } from 'tiny-typed-emitter';
+
 export interface ThrottleBatchOptions<A, B> {
   threshold?: number;
   createInitialBatch?: () => B;
@@ -72,4 +74,86 @@ export default function throttleBatch<A extends any[], B extends any[]>(
       }
     }
   };
+}
+
+interface BatchAsyncEvent<T> {
+  completed: () => void;
+  feed: (item: T) => void;
+  batch: (items: T[]) => void;
+  error: (error: any) => void;
+}
+
+export class BatchAsync<T> extends TypedEmitter<BatchAsyncEvent<T>> {
+  stack: T[][] = [];
+  batch: T[] = [];
+  processing = false;
+  completed = false;
+
+  private timeout: any;
+
+  constructor(
+    batchFn: (items: T[]) => Promise<any>,
+    batchSize = 1000,
+    private debound = 500
+  ) {
+    super();
+    this.on('feed', data => {
+      this.touch();
+      this.batch.push(data);
+      if (this.batch.length == batchSize) {
+        this.emit('batch', this.batch);
+        this.batch = [];
+      }
+    });
+    this.on('batch', async (items: any[]) => {
+      if (this.processing) {
+        this.stack.push(items);
+        return;
+      }
+      this.processing = true;
+      try {
+        await batchFn(items);
+      } catch (err) {
+        this.emit('error', err);
+      } finally {
+        this.processing = false;
+        if (this.stack.length > 0) {
+          this.emit('batch', this.stack.shift() as T[]);
+        } else {
+          if (this.completed) this.emit('completed');
+        }
+      }
+    });
+
+    this.on('completed', () => {
+      if (this.timeout) clearTimeout(this.timeout);
+    });
+  }
+
+  complete() {
+    this.completed = true;
+  }
+
+  feed(item: T) {
+    this.emit('feed', item);
+  }
+
+  private touch() {
+    if (this.timeout) clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => {
+      if (this.processing == false && this.batch.length > 0) {
+        this.emit('batch', this.batch);
+        this.batch = [];
+      }
+      if (
+        this.processing == false &&
+        this.batch.length == 0 &&
+        this.completed
+      ) {
+        this.emit('completed');
+      } else {
+        this.touch();
+      }
+    }, this.debound);
+  }
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,27 +1,53 @@
-import throttleBatch from '../src';
+import { BatchAsync } from '../src';
 
 const sleep = (ms: number) =>
   new Promise(resolve => setTimeout(resolve, ms * 1000));
 
-test('Batch', async () => {
-  const fn = throttleBatch(console.log, {
-    threshold: 1000,
-    batchSize: 2,
+// test('Batch', async () => {
+//   const fn = throttleBatch(console.log, {
+//     threshold: 1000,
+//     batchSize: 2,
+//   });
+
+//   fn(1);
+//   fn(2);
+//   fn(3);
+
+//   setTimeout(() => {
+//     fn(4);
+//     fn(5);
+//     fn(6);
+//   }, 1100);
+//   setTimeout(() => {
+//     fn(7);
+//     fn(8);
+//     fn(9);
+//   }, 2200);
+//   await sleep(10);
+// }, 10000);
+
+test('Bathc Async', async () => {
+  const batchAsync = new BatchAsync(async (items: string[]) => {
+    console.log(items);
+    await sleep(0.1);
+  }, 20);
+
+  for (var i = 0; i < 500; i++) {
+    batchAsync.feed(i.toString());
+  }
+
+  await sleep(0.2);
+  for (var i = 10; i < 20; i++) {
+    batchAsync.feed(i.toString());
+  }
+  await sleep(2);
+  for (var i = 20; i < 30; i++) {
+    batchAsync.feed(i.toString());
+  }
+  batchAsync.complete();
+  console.log('set complete');
+  batchAsync.on('completed', () => {
+    console.log('complete');
   });
-
-  fn(1);
-  fn(2);
-  fn(3);
-
-  setTimeout(() => {
-    fn(4);
-    fn(5);
-    fn(6);
-  }, 1100);
-  setTimeout(() => {
-    fn(7);
-    fn(8);
-    fn(9);
-  }, 2200);
-  await sleep(10);
-}, 10000);
+  await sleep(20);
+}, 30000);
